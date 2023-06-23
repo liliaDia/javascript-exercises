e33ca26009248e83d9af9881d2f2189f146524af
lilia <liliayamamoto88@gmail.com>
2023-06-22 19:28:40 -1000

    did exercises 1-5

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num < 0) {
+    return Error("ERROR");
+  }
+  return "hey".repeat(num);
 };
-
+console.log(repeatString("hey", 5));
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,32 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, value) {
+  for (let i = 0; i < array.length; i++) {
+    if (arguments.length > 2) {
+      for (let j = 1; j < arguments.length; j++) {
+        if (arguments[j] === array[i]) {
+          array.splice(i, 1);
+        }
+      }
+    }
+    if (array[i] === value) {
+      array.splice(i, 1);
+    }
+  }
+  return array;
 };
+
+// const removeFromArray = function (array, ...args) {
+
+//   const newArray = [];
+
+//   array.forEach((item) => {
+
+//     if (!args.includes(item)) {
+//       newArray.push(item);
+//     }
+//   });
+
+//   return newArray;
+// };
 
 // Do not edit below this line
 module.exports = removeFromArray;
